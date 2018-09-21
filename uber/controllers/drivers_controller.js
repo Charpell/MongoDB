15568f822849b579ebfc8d6e58db82d53ecc3a1c
Charpell <charpellumeh@gmail.com>
2018-09-21 12:29:56 +0100

    Complete handelers for Create Edit and Update driver

--- uber/controllers/drivers_controller.js
+++ uber/controllers/drivers_controller.js
@@ -1,5 +1,39 @@
+const Driver = require('../models/Driver');
+
 module.exports = {
   greeting(req, res) {
     res.send({ hi: 'there' })
+  },
+
+  create(req, res) { 
+    const driverProps = req.body;
+
+    Driver.create(driverProps)
+      .then(driver => res.send(driver))
+      .catch(error => {
+        res.status(422).send({ error: error.message });
+      })
+  },
+
+  edit(req, res) {
+    const driverId = req.params.id;
+    const driverProps = req.body;
+
+    Driver.findByIdAndUpdate({ _id: driverId }, driverProps)
+      .then(() => Driver.findById({ _id: id }))
+      .then(driver => res.send(driver))
+      .catch(error => {
+        res.status(422).send({ error: error.message });
+      });
+  },
+
+  delete(req, res) {
+    const driverId = req.params.id;
+
+    Driver.findByIdAndRemove({ _id: driverId })
+      .then(driver => res.status(204).send(driver))
+      .catch(error => {
+        res.status(422).send({ error: error.message });
+      });
   }
 };
